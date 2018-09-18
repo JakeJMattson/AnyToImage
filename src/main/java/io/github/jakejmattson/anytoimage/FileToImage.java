eeb6fbaf27de31b17ca7bdae9fa6dab120d801d2
Jake <JakeJMattson@gmail.com>
2018-09-18 12:03:10 -0500

    Improve memory performance
    Improved memory performance when converting files to images. Look ahead to calculate image size in advance and write data to it directly during processing instead of collecting all pixels at once.

--- src/main/java/io/github/jakejmattson/anytoimage/FileToImage.java
+++ src/main/java/io/github/jakejmattson/anytoimage/FileToImage.java
@@ -26,7 +26,7 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.*;
-import java.util.List;
+import java.util.*;
 import java.util.stream.*;
 
 /**
@@ -36,6 +36,13 @@ import java.util.stream.*;
  */
 final class FileToImage
 {
+	private static ByteArrayOutputStream stream = new ByteArrayOutputStream();
+	private static BufferedImage image;
+	private static int row;
+	private static int col;
+
+	private static final int CHANNEL_COUNT = 3;
+
 	private FileToImage()
 	{
 		throw new IllegalStateException("Stateless class");
@@ -51,81 +58,111 @@ final class FileToImage
 	 */
 	static boolean convert(List<File> inputFiles, File outputFile)
 	{
-		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		int bytes = calculatePixelCount(inputFiles);
+		int dims = (int) Math.ceil(Math.sqrt(bytes / CHANNEL_COUNT));
+
+		//No data to write
+		if (dims == 0)
+			return false;
+
+		//(Re)initialize fields
+		image = new BufferedImage(dims, dims, BufferedImage.TYPE_INT_RGB);
+		row = 0;
+		col = 0;
 
 		//Get file bytes
 		for (File file : inputFiles)
 			if (file.exists())
 				if (file.isDirectory())
-					directoryToBytes(stream, file);
-				else if (file.isFile())
-					fileToBytes(stream, file, file.getName());
+					directoryToBytes(file);
+				else
+					fileToBytes(file, file.getName());
 
-		//Create pixels from file information
-		int[] pixels = bytesToPixels(stream.toByteArray());
+		//Write remaining data onto the image
+		finalizeStream();
 
 		//Create image and return success status
-		return createImage(pixels, outputFile);
+		return createImage(outputFile);
+	}
+
+	private static int calculatePixelCount(List<File> inputFiles)
+	{
+		int byteCount = 0;
+
+		for (File file : inputFiles)
+			if (file.exists())
+				if (file.isDirectory())
+				{
+					String parentDir = file.getName();
+					List<Path> paths = walkDirectory(file);
+
+					for (Path path : paths)
+					{
+						String fullPath = path.toString();
+						String fileName = fullPath.substring(fullPath.indexOf(parentDir));
+
+						byteCount += pixelSizeOfFile(path.toFile(), fileName);
+					}
+				}
+				else
+					byteCount += pixelSizeOfFile(file, file.getName());
+
+		return byteCount;
+	}
+
+	private static int pixelSizeOfFile(File file, String fileName)
+	{
+		int byteCount = 0;
+
+		byteCount += 1;
+		byteCount += fileName.getBytes().length;
+		byteCount += 4;
+		byteCount += file.length();
+
+		return byteCount;
 	}
 
 	/**
 	 * Turn directory into bytes and preserve directory structure.
 	 *
-	 * @param stream
-	 *            Byte stream currently open
-	 * @param file
+	 * @param dir
 	 *            Directory to extract bytes from
 	 */
-	private static void directoryToBytes(ByteArrayOutputStream stream, File file)
+	private static void directoryToBytes(File dir)
 	{
-		//Get selected directory
-		String parentDir = file.getName();
+		String parentDir = dir.getName();
+		List<Path> paths = walkDirectory(dir);
 
-		try (Stream<Path> files = Files.walk(file.toPath()))
+		for (Path path : paths)
 		{
-			//Get all files from the directory and its sub-directories
-			List<Path> paths = files.filter(Files::isRegularFile).collect(Collectors.toList());
+			//Construct arguments
+			String fullPath = path.toString();
+			String fileName = fullPath.substring(fullPath.indexOf(parentDir));
 
-			for (Path path : paths)
-			{
-				//Construct arguments
-				String fullPath = path.toString();
-				String fileName = fullPath.substring(fullPath.indexOf(parentDir));
-
-				//Retrieve bytes from each file
-				fileToBytes(stream, path.toFile(), fileName);
-			}
-		}
-		catch (IOException e)
-		{
-			DialogDisplay.displayException(e, "Unable to walk directory: " + file.toString());
+			//Retrieve bytes from each file
+			fileToBytes(path.toFile(), fileName);
 		}
 	}
 
 	/**
 	 * Turn file into bytes.
 	 *
-	 * @param stream
-	 *            Byte stream currently open
 	 * @param file
 	 *            File to extract bytes from
 	 * @param fileName
-	 *            Name (with / without folder structure)
+	 *            Name of file (with folder structure if file was in directory)
 	 */
-	private static void fileToBytes(ByteArrayOutputStream stream, File file, String fileName)
+	private static void fileToBytes(File file, String fileName)
 	{
 		try
 		{
 			//Acquire file information
-			byte[] name = fileName.getBytes();
-			byte[] data = Files.readAllBytes(file.toPath());
-			byte[] nameLength = new byte[] {(byte) name.length};
-			byte[] dataLength = ByteUtils.intToBytes(data.length, 4);
+			stream.write((byte) fileName.length());
+			stream.write(fileName.getBytes());
+			stream.write(ByteUtils.intToBytes((int) file.length(), 4));
+			stream.write(Files.readAllBytes(file.toPath()));
 
-			stream.write(nameLength);
-			stream.write(name);
-			stream.write(dataLength);
-			stream.write(data);
+			writeDataToImage();
 		}
 		catch (IOException e)
 		{
@@ -133,78 +170,85 @@ final class FileToImage
 		}
 	}
 
-	/**
-	 * Covert file bytes into an integer (pixel).
-	 *
-	 * @param bytes
-	 *            All bytes to be converted
-	 * @return All pixels
-	 */
-	private static int[] bytesToPixels(byte[] bytes)
+	private static List<Path> walkDirectory(File dir)
 	{
-		final int RGB = 3, RGBA = 4;
-
-		//Number of image channels
-		int numOfChannels = RGB;
-
-		//Total number of characters to convert
-		int byteCount = bytes.length;
-
-		//Determine total number of pixels
-		int pixelCount = (int) Math.pow(Math.ceil(Math.sqrt(byteCount / numOfChannels)), 2);
-		int[] pixels = new int[pixelCount];
-
-		//Read text in groups of [channel count]
-		for (int i = 0; i < byteCount; i += numOfChannels)
+		try (Stream<Path> files = Files.walk(dir.toPath()))
 		{
-			//Array of current pixel info
-			byte[] pixel = new byte[numOfChannels];
+			return files.filter(Files::isRegularFile).collect(Collectors.toList());
+		}
+		catch (IOException e)
+		{
+			DialogDisplay.displayException(e, "Unable to walk directory: " + dir.toString());
+			return Collections.emptyList();
+		}
+	}
 
-			//Read info from group into each channel
-			for (int j = 0; j < numOfChannels; j++)
-				if (i + j < byteCount)
-					pixel[j] = bytes[i + j];
-				else
-					break;
+	private static void writeDataToImage()
+	{
+		byte[] fileBytes = stream.toByteArray();
+		int i;
 
-			//Store current pixel into pixel array
-			pixels[i / numOfChannels] = ByteUtils.bytesToInt(pixel);
+		for (i = 0; i < fileBytes.length - CHANNEL_COUNT; i += CHANNEL_COUNT)
+		{
+			byte[] pixelData = {fileBytes[i], fileBytes[i + 1], fileBytes[i + 2]};
+			int pixel = ByteUtils.bytesToInt(pixelData);
+
+			image.setRGB(row, col, pixel);
+
+			row++;
+
+			if (row == image.getWidth())
+			{
+				row = 0;
+				col++;
+			}
 		}
 
-		return pixels;
+		stream.reset();
+
+		for ( ; i < fileBytes.length; i++)
+		{
+			stream.write(fileBytes[i]);
+		}
+	}
+
+	private static void finalizeStream()
+	{
+		byte[] finalPixelData = new byte[CHANNEL_COUNT];
+		byte[] partialPixel = stream.toByteArray();
+		System.arraycopy(partialPixel, 0, finalPixelData, 0, partialPixel.length);
+		int pixel = ByteUtils.bytesToInt(finalPixelData);
+		image.setRGB(row, col, pixel);
+
+		stream.reset();
 	}
 
 	/**
 	 * Create an image from pixels and save to disk.
 	 *
-	 * @param pixels
-	 *            The pixel data to be written to the image
 	 * @param output
 	 *            The desired file location of the output image
 	 * @return Whether or not the image was written to disk
 	 */
-	private static boolean createImage(int[] pixels, File output)
+	private static boolean createImage(File output)
 	{
-		//Calculate image dimensions
-		int dims = (int) Math.ceil(Math.sqrt(pixels.length));
-
-		if (dims == 0)
-			return false;
-
-		//Store pixel values in image
-		BufferedImage image = new BufferedImage(dims, dims, BufferedImage.TYPE_INT_RGB);
-		image.setRGB(0, 0, dims, dims, pixels, 0, dims);
+		boolean status = false;
 
 		try
 		{
 			//Write image to file
 			ImageIO.write(image, "png", output);
-			return true;
+			status = true;
 		}
 		catch (IOException e)
 		{
 			DialogDisplay.displayException(e, "Error creating image!");
-			return false;
 		}
+		finally
+		{
+			image = null;
+		}
+
+		return status;
 	}
 }
